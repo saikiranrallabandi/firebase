b9032f5edeedeb8adbf885c0d976b13cdc97e8b6
saikiranrallabandi <42328448+saikiranrallabandi@users.noreply.github.com>
2020-08-02 14:28:05 -0700

    Update Firebase.js

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -6,13 +6,7 @@ import "firebase/storage";
 const settings = {timestampsInSnapshots: true};
 
 const config = {
-  apiKey: "AIzaSyAwxquuAqcXkeBvHgSVFT03KB7uj9M9quY",
-  authDomain: "icloudsigmaxm.firebaseapp.com",
-  databaseURL: "https://icloudsigmaxm.firebaseio.com",
-  projectId: "icloudsigmaxm",
-  storageBucket: "icloudsigmaxm.appspot.com",
-  messagingSenderId: "332515964828",
-  appId: "1:332515964828:web:8ea4f13e658a3bf59e3a59"
+  
 }
 
 
